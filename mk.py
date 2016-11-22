4f73f55002a89acae1d9987709496a02baa8c80c
restlessankyy <rajankit749@gmail.com>
2016-11-22 04:18:33 -0800

    Add files via upload

---
+++ mk.py
@@ -0,0 +1,13 @@
+import re
+line = "Cats are smarter than dogs"
+matchObj = re.match(r'dogs',line,re.M|re.I)
+if matchObj:
+    print "match --> matchObj.group():",matchObj.group()
+else :
+    print "No Match!!!"
+searchObj = re.search(r'dogs',line,re.M|re.I)
+if searchObj:
+    print "search --> searchObj.group():",searchObj.group()
+else :
+    print "Nothing Found !!!"
+        

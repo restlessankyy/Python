eb5fefb2e57979ddf2e3860240f47bf8a90dc6ed
restlessankyy <rajankit749@gmail.com>
2016-11-23 02:25:06 -0800

    Add files via upload

---
+++ CheckBox2RB.py
@@ -0,0 +1,48 @@
+from Tkinter import *
+from tkMessageBox import *
+
+def changeLabel():
+    name="Thanks for the click" + yourName.get()
+    labelText.set(name)
+    msg="My Name is "+ yourName.get()
+    yourName.delete(0,END)
+    yourName.insert(0,msg)
+def hasClicked():
+        
+   radioValue = relStatus.get()
+   showinfo("You Clicked ",radioValue)
+window1 = Tk()
+window1.title("GUI Example")
+window1.geometry("450x300+200+200")
+
+#CreateLabel
+labelText=StringVar()
+labelText.set("Click Button")
+label1 = Label(window1, textvariable=labelText, height=4)
+label1.pack()
+
+#CreateCheckBox
+checkBoxVal = IntVar()
+checkBox1 = Checkbutton(window1,variable=checkBoxVal,text="Happy?!!!")
+checkBox1.pack()
+
+
+#CreateInput
+custName = StringVar()
+yourName = Entry(window1,textvariable=custName)
+yourName.pack()
+
+#CreateRadio Option
+relStatus= StringVar()
+relStatus.set(None)
+radio1=Radiobutton(window1, text="Single",value="Single",variable=relStatus, command=hasClicked)
+radio2=Radiobutton(window1, text="Married",value="Married",variable=relStatus, command=hasClicked)
+radio1.pack()
+radio2.pack()
+
+
+
+#createButton
+button1=Button(window1,text="Click Here",width=20,command=changeLabel)
+button1.pack(side="bottom")
+window1.mainloop()

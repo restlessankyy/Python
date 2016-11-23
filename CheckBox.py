1440cbea3a5c29f231e7442c9bb9eb45810ca7d1
restlessankyy <rajankit749@gmail.com>
2016-11-23 02:06:51 -0800

    Add files via upload

--- CheckBox.py
+++ CheckBox.py
@@ -1,6 +1,10 @@
 from Tkinter import *
 from tkMessageBox import *
 
+def changeLabel():
+    pass
+def hasClicked():
+   pass
 window1 = Tk()
 window1.title("GUI Example")
 window1.geometry("450x300+200+200")
@@ -16,4 +20,23 @@ checkBoxVal = IntVar()
 checkBox1 = Checkbutton(window1,variable=checkBoxVal,text="Happy?!!!")
 checkBox1.pack()
 
+
+#CreateInput
+custName = StringVar()
+yourName = Entry(window1,textvariable=custName)
+yourName.pack()
+
+#CreateRadio Option
+relStatus= StringVar()
+relStatus.set(None)
+radio1=Radiobutton(window1, text="Single",value="Single",variable=relStatus, command=hasClicked)
+radio2=Radiobutton(window1, text="Married",value="Married",variable=relStatus, command=hasClicked)
+radio1.pack()
+radio2.pack()
+
+
+
+#createButton
+button1=Button(window1,text="Click Here",width=20,command=changeLabel)
+button1.pack(side="bottom")
 window1.mainloop()
